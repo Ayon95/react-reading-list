818dc5e5827262826791962dad2b0c481456b9f2
Ayon95 <mushfiqurrahman78@yahoo.com>
2021-03-30 19:15:40 -0400

    Initial commit and created a BookContext

---
+++ src/contexts/BookContext.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { v4 } from "uuid";
+
+const BookContext = React.createContext();
+
+function BookContextProvider(props) {
+  const [books, setBooks] = useState([
+    { title: "The way of kings", author: "Brandon Sanderson", id: 1 },
+    { title: "The final empire", author: "Brandon Sanderson", id: 2 },
+  ]);
+
+  function addBook(title, author) {
+    setBooks([...books, { title, author, id: v4() }]);
+  }
+
+  function removeBook(id) {
+    setBooks(books.filter((book) => book.id !== id));
+  }
+
+  const values = { books, addBook, removeBook };
+
+  return (
+    <BookContext.Provider value={values}>{props.children}</BookContext.Provider>
+  );
+}
+
+export default BookContextProvider;
